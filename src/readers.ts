63dbb57d42b63acf568ef920937e533aab2a2acb
Galyfray <galyfray@gmail.com>
2023-03-22 21:09:20 +0100

    reworked readers

--- src/readers.ts
+++ src/readers.ts
@@ -4,20 +4,25 @@ import path from "path"
 import { type CCReport } from "./types";
 import {IsCCReport} from "./types.guard";
 
-export async function CCReader(cc_path:string): Promise<CCReport>{
-    const RESOLVED:string = path.resolve(cc_path);
-    let data: unknown = undefined
-    try {
-        data = JSON.parse(await fs.promises.readFile(RESOLVED, {encoding:"utf-8"}));
-    } catch(err) {
-        if (err instanceof SyntaxError){
-            throw err
+type Reader<T> = (path:string)=> Promise<T>
+
+function ReaderFactory<T>(checker : (obj:unknown)=>obj is T): Reader<T>{
+    return async (cc_path:string): Promise<T> => {
+        const RESOLVED:string = path.resolve(cc_path);
+        let data: unknown = undefined
+        try {
+            data = JSON.parse(await fs.promises.readFile(RESOLVED, {encoding:"utf-8"}));
+        } catch(err) {
+            if (err instanceof SyntaxError){
+                throw err
+            }
+            throw new Error(`Error: Unable to read the file ${RESOLVED}`);
         }
-        throw new Error(`Error: Unable to read the file ${RESOLVED}`);
+        if ( checker(data)){
+            return data
+        }
+        
+        throw new Error("Parsing Error: The data found in the provided file does not has the expected structure.");
     }
-    if ( IsCCReport(data)){
-        return data
-    }
-    
-    throw new Error("Parsing Error: The data found in the provided file does not has the expected structure.");
 }
+export const CCReader:Reader<CCReport> = ReaderFactory<CCReport>(IsCCReport);
